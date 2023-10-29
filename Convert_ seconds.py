2ade9963917c9a23ee9f6a01119c7a17a95e5eeb
AMARENDRA-EEDIGA <138709437+AMARENDRA-EEDIGA@users.noreply.github.com>
2023-10-29 23:24:45 +0530

    Update Convert_ seconds.py

--- Convert_ seconds.py
+++ Convert_ seconds.py
@@ -4,13 +4,26 @@ def convert_seconds(seconds):
   remaining_seconds = seconds - hours * 3600 - minutes * 60
   return hours, minutes, remaining_seconds
 
+# Calling the function and storing it into the a variable called result
 result = convert_seconds(5000)
+# Printing the result
 print(result)
-hours, minutes, seconds =result
-print(hours, minutes, seconds)
-
 
 OUTPUT:
-
 (1, 23, 20)
+
+# Here these 3 elements( hours, minutes, seconds ) are stored into the result
+hours, minutes, seconds =result
+# printing the elements using normal print method
+print(hours, minutes, seconds)
+
+OUTPUT:
 1 23 20
+
+# Here we are calling the function with giving the new seconds value
+hours, minutes, seconds = convert_seconds(1000)
+# printing the result
+print(hours, minutes, seconds)
+
+OUTPUT:
+0 16 40
